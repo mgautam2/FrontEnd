450e318934c39943eb829fd43d4b2d41913f5d29
mengwang <mengw3@illinois.edu>
2020-08-09 23:42:34 -0400

    finish LoginAsUser page

--- src/screens/NewAdmin/LoginAsUser/index.js
+++ src/screens/NewAdmin/LoginAsUser/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import { CTFragment, CTHeading } from 'layout';
+import React, { useState } from 'react';
+import { CTFragment, CTHeading, CTForm, CTInput } from 'layout';
+import { user } from 'utils';
+import { Button } from 'pico-ui';
 import { connectWithRedux } from '../controllers';
 import './index.scss';
 
@@ -11,11 +13,51 @@ function LoginAsUserWithRedux() {
     offsetTop: 30
   });
 
+  const [emailAddress, setEmailAddress] = useState('');
+
+  const handleEmailInput = ({ target: { value }}) => setEmailAddress(value);
+
+  const onSignIn = () => {
+    user.loginAsAccountSignIn(emailAddress);
+  };
+
   return (
     <CTFragment>
       <CTHeading {...headingProps} />
-      <CTFragment padding={[0, 30]}>
-        Here is LoginAsUser page
+      <CTFragment padding={[0, 50]}>
+        {user.isLoginAsAccount ? (
+          <CTForm
+            id="Login-As-User" 
+            padding={[0, 30]}
+            heading="Login As User"
+            onSave={user.loginAsAccountSignOut}
+            onSaveButtonText='Sign Out'
+            className="loginAsUser-container"
+          >
+            <div>
+              Signed in as <strong>{user.getLoginAsUserInfo().emailId}</strong>.
+            </div>
+          </CTForm>
+        ) : (
+          <CTForm
+            id="Login-As-User" 
+            padding={[0, 30]}
+            heading="Login As User"
+            details="Input Your Email Address"
+            onSave={onSignIn}
+            onSaveButtonText='Sign In'
+            className="loginAsUser-container"
+          >     
+            <CTInput
+              required
+              id="email-address"
+              label="Email"
+              placeholder="Email Address"
+              value={emailAddress}
+              onChange={handleEmailInput}
+            />
+          </CTForm>
+        )}
       </CTFragment>
     </CTFragment>
   );
