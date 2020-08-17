aacce62454195f8fb7d420bb70793cafbb706460
Shawn Sui <ruihua.sui@gmail.com>
2020-08-16 22:59:34 -0500

    Trivial updates for Terms component

--- src/screens/NewAdmin/Terms/index.js
+++ src/screens/NewAdmin/Terms/index.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect }from 'react';
-import { CTFragment, CTHeading, CTAutoComplete, CTFormHelp, CTText } from 'layout';
-import { api } from 'utils';
 import _ from 'lodash';
 import { Typography, TextField, Grid } from "@material-ui/core";
-import GeneralTable from '../Components/GeneralTable';
+import { CTFragment, CTHeading, CTAutoComplete, CTFormHelp } from 'layout';
+import { api } from 'utils';
 import { connectWithRedux } from '../controllers';
+import GeneralTable from '../Components/GeneralTable';
 import './index.scss';
 
 function TermsWithRedux() {
@@ -54,21 +54,26 @@ function TermsWithRedux() {
     );
   };
 
-  const handleChangeDate = (e, props, isStart) => {
-    return (
-      isStart 
-      ? props.onRowDataChange({
-          ...props.rowData,
-          startDate: (e.target.value !== '' ? new Date(e.target.value).toISOString() : undefined)
-      })
-      : props.onRowDataChange({
-          ...props.rowData,
-          endDate: (e.target.value !== '' ? new Date(e.target.value).toISOString() : undefined)
-      })
-    );
-  };
 
   const editDate = (props, isStart) => {
+    const inputProps = isStart
+    ? { max: (props.rowData.endDate || '').substring(0, 10) } 
+    : { min: (props.rowData.startDate || '').substring(0, 10) };
+
+    const handleChangeDate = (e) => {
+      return (
+        isStart 
+        ? props.onRowDataChange({
+            ...props.rowData,
+            startDate: (e.target.value !== '' ? new Date(e.target.value).toISOString() : undefined)
+        })
+        : props.onRowDataChange({
+            ...props.rowData,
+            endDate: (e.target.value !== '' ? new Date(e.target.value).toISOString() : undefined)
+        })
+      );
+    };
+    
     return (
       <Grid item xs={12} sm={12} md={12} lg={6}>
         <TextField
@@ -79,10 +84,8 @@ function TermsWithRedux() {
           InputLabelProps={{
             shrink: true,
           }}
-          inputProps={isStart
-            ? { max: (props.rowData.endDate || '').substring(0, 10) } 
-            : { min: (props.rowData.startDate || '').substring(0, 10) }}
-          onChange={e => handleChangeDate(e, props, isStart)}
+          inputProps={inputProps}
+          onChange={handleChangeDate}
           value={isStart
           ? (props.rowData.startDate || '').substring(0, 10)
           : (props.rowData.endDate || '').substring(0, 10)}
@@ -99,32 +102,22 @@ function TermsWithRedux() {
     { title: 'Start Date',
       field: 'startDate',
       validate: rowData => rowData.startDate !== undefined,
-      render: rowData => {
-        return renderDate(rowData.startDate);
-      },
-      editComponent: props => {
-        return editDate(props, true);
-      }
+      render: rowData => renderDate(rowData.startDate),
+      editComponent: props => editDate(props, true)
     },
     { title: 'End Date',
       field: 'endDate',
       validate: rowData => rowData.endDate !== undefined,
-      render: rowData => {
-        return renderDate(rowData.endDate);
-      },
-      editComponent: props => {
-        return editDate(props, false);
-      }
+      render: rowData => renderDate(rowData.endDate),
+      editComponent: props => editDate(props, false)
     },
     { title: 'University', 
       field: 'id',
-      render: () => {
-        return (
-          <Typography variant="body1">
-            {currUniversity.name}
-          </Typography>
-        );
-      },
+      render: () => (
+        <Typography variant="body1">
+          {currUniversity.name}
+        </Typography>
+      ),
       editable: false
     },
   ];
@@ -133,7 +126,6 @@ function TermsWithRedux() {
     const temp = _.find(universities, ['id', value]);
     setCurrUniversity(temp);
   };
-
 
   return (
     <CTFragment className='terms-container'>
@@ -170,6 +162,6 @@ function TermsWithRedux() {
 }
 
 export const Terms = connectWithRedux(
-    TermsWithRedux,
-    []
+  TermsWithRedux,
+  []
 );
